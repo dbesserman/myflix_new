5f9df59a00bccb394e66cb48022fb87a8c19c1a3
David Besserman <david.besserman@gmail.com>
2017-11-26 16:00:46 +0100

    home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Video.create([
+  { title: 'Family Guy', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/family_guy.jpg' },
+  { title: 'Futurama', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama.jpg' },
+  { title: 'Monk', small_cover_url: '/tmp/monk.jpg', large_cover_url: '/tmp/monk_large.jpg' },
+  { title: 'South Park', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/south_park.jpg' }
+])
